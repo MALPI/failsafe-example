74897cfba892204b7d00e5f34cdeacca631d5c33
mpickhan <malte.pickhan@zalando.de>
2017-07-20 13:21:31 +0200

    Started implmenetation of payment service example

---
+++ solvency-service/src/main/java/de/mpickhan/solvencyservice/SolvencyServiceApplication.java
@@ -0,0 +1,12 @@
+package de.mpickhan.solvencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvencyServiceApplication.class, args);
+	}
+}
